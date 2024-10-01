add49c98409ed0fa9004bec4ff022f25d4838e42
FelipeLarsen <feliper.larsen@gmail.com>
2024-10-01 18:07:31 -0300

    API Fetch

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import styles from './Header.module.css'
 import { Link } from 'react-router-dom'
-import Dog from '../Assets/dogs.svg?react'
+import Dogs from '../Assets/dogs.svg?react'
 
 const Header = () => {
   return (
     <header className={styles.header}>
       <nav className={`${styles.nav} container`}>
         <Link className={styles.logo} to='/' aria-label='Dogs - Home'>
-          <Dog />
+          <Dogs />
         </Link>
         <Link className={styles.login} to='/login'>Login / Criar</Link>
       </nav>
