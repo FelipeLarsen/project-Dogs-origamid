be277437527a0e0ae9784225efd84003de701a30
FelipeLarsen <feliper.larsen@gmail.com>
2024-10-02 15:33:55 -0300

    Login Background

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -5,6 +5,7 @@ import LoginCreateUser from './LoginCreateUser'
 import LoginPasswordLost from './LoginPasswordLost'
 import LoginPasswordReset from './LoginPasswordReset'
 import { UserContext } from '../../UserContext'
+import styles from './Login.module.css'
 
 const Login = () => {
   const { login } = React.useContext(UserContext)
@@ -12,14 +13,16 @@ const Login = () => {
   if (login === true) return <Navigate to="/my_account" />
 
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<LoginForm />} />
-        <Route path="create_user" element={<LoginCreateUser />} />
-        <Route path="lost_password" element={<LoginPasswordLost />} />
-        <Route path="password_reset" element={<LoginPasswordReset />} />
-      </Routes>
-    </div>
+    <section className={styles.login}>
+      <div className={styles.forms}>
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <Route path="create_user" element={<LoginCreateUser />} />
+          <Route path="lost_password" element={<LoginPasswordLost />} />
+          <Route path="password_reset" element={<LoginPasswordReset />} />
+        </Routes>
+      </div>
+    </section>
   )
 }
 
