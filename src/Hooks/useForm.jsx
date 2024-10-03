bbb853d4b0ff1aabf9236e29b68b2ed2925c844e
FelipeLarsen <feliper.larsen@gmail.com>
2024-10-03 11:40:08 -0300

    Update

--- src/Hooks/useForm.jsx
+++ src/Hooks/useForm.jsx
@@ -5,6 +5,10 @@ const validation = {
         regex: /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/,
         message: 'Preencha com um email válido'
     },
+    password: {
+        regex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/,
+        message: 'A senha precisa ter 1 caracter maiúsculo, 1 minúsculo e 1 caractere especial. Com no mínimo 8 caracteres'
+    }
 }
 
 const useForm = (type) => {
