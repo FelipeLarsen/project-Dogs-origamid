f4b8d3a2372e6fa18eead5a740600579e28dc6ce
FelipeLarsen <feliper.larsen@gmail.com>
2024-10-08 11:41:30 -0300

    Photo Post

--- src/Hooks/useForm.jsx
+++ src/Hooks/useForm.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 
-const validation = {
+const types = {
     email: {
         regex: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-        message: 'Preencha com um email válido'
+        message: 'Preencha com um email válido.'
     },
     password: {
         regex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/,
-        message: 'A senha precisa ter 1 caracter maiúsculo, 1 minúsculo e 1 caractere especial. Com no mínimo 8 caracteres'
+        message: 'A senha precisa ter 1 caracter maiúsculo, 1 minúsculo e 1 caractere especial. Com no mínimo 8 caracteres.'
+    },
+    number: {
+        regex: /^\d+$/,
+        message: 'Utilize números apenas.'
     }
 }
 
